65ac521cdef7460b41bc54e4c9899f53d65e8a2c
susmitamitu <deb687362@gmail.com>
2026-09-16 10:57:35 +0600

    Add files via upload

---
+++ Lab 2/YourWork/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct student
+{
+int ID;
+float CGPA;
+};
+int main()
+{
+student s1;
+s1.ID=101;
+s1.CGPA=3.75;
+cout<<"ID:"<<s1.ID<<",CGPA"<<s1.CGPA<<endl;
+return 0;
+}
